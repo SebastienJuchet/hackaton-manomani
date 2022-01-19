70d3f1e8c4bfb1e2b1cc17f8222e2129bd2055e2
Sebastien Juchet <sebastien.juchet@gmail.com>
2022-01-19 15:18:59 +0100

    correct button

--- profil.php
+++ profil.php
@@ -8,8 +8,11 @@
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
-<body class="profil">
-<a href="secondo.php" class="btn btn-secundo"></a>
+<body>
+<div>
+    <img src="/assets/user_mano.png" class="profil" alt="" />
+    <a href="secondo.php" class="btn btn-secundo"></a>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
