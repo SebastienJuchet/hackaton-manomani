384792834524db85ca654f2e15fc29d9accd3b07
Sebastien Juchet <sebastien.juchet@gmail.com>
2022-01-19 14:53:08 +0100

    ajout button secundo

--- profil.php
+++ profil.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
-<body>
-
+<body class="profil">
+<a href="secondo.php" class="btn btn-secundo"></a>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
