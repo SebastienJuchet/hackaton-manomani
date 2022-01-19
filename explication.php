cc9b653232639ea8e47c9ec68ffaa1e5442b2ebf
Sebastien Juchet <sebastien.juchet@gmail.com>
2022-01-19 22:30:50 +0100

    corrige passage a la ligne

--- explication.php
+++ explication.php
@@ -10,35 +10,35 @@ require 'component/header.php'
 
 <h2 class="h2 text-center">La marketplace Communautaire - Eco Responsable by ManoMano</h2>
 
-    <figure class="figure">
-        <blockquote class="blockquote">
-            <p class="text-mano font-size">Le principe</p>
-        </blockquote>
-        <figcaption class="blockquote-footer">
-            Vous avez acheté un produit consommable chez Mano Mano et il vous en reste ! Plutôt que de le jeter ou le laisser dans le fond de votre garage,
-            nous vous proposons d'en faire profiter la communauté Mano Mano à un prix plafonné à 50% de son prix initial (hors promo).
-            Cette vente est comptabilisée dans votre cagnotte Ségundo Mano.
-        </figcaption>
-    </figure>
-
-    <figure class="figure">
-        <blockquote class="blockquote">
-            <p class="text-mano font-size">Comment utiliser ma cagnotte ?</p>
-        </blockquote>
-        <figcaption class="blockquote-footer">
-            Vous avez une année pour utiliser la cagnotte sur le site ManoMano.
-        </figcaption>
-    </figure>
-
-    <figure class="figure">
-        <blockquote class="blockquote">
-            <p class="text-mano font-size">Comment se passe la livraison des produits vendus ?</p>
-        </blockquote>
-        <figcaption class="blockquote-footer">
-            Les produits sont remis soit :
-                En main Propre soit au travers de notre partenaire transport CoColis le transport responsable entre particuliers</li>.
-        </figcaption>
-    </figure>
+<figure class="figure">
+    <blockquote class="blockquote">
+        <p class="text-mano font-size">Le principe</p>
+    </blockquote>
+    <figcaption class="blockquote-footer">
+        Vous avez acheté un produit consommable chez Mano Mano et il vous en reste ! Plutôt que de le jeter ou le laisser dans le fond de votre garage,
+        nous vous proposons d'en faire profiter la communauté Mano Mano à un prix plafonné à 50% de son prix initial (hors promo).
+        Cette vente est comptabilisée dans votre cagnotte Ségundo Mano.
+    </figcaption>
+</figure>
+<br>
+<figure class="figure">
+    <blockquote class="blockquote">
+        <p class="text-mano font-size">Comment utiliser ma cagnotte ?</p>
+    </blockquote>
+    <figcaption class="blockquote-footer">
+        Vous avez une année pour utiliser la cagnotte sur le site ManoMano.
+    </figcaption>
+</figure>
+<br>
+<figure class="figure">
+    <blockquote class="blockquote">
+        <p class="text-mano font-size">Comment se passe la livraison des produits vendus ?</p>
+    </blockquote>
+    <figcaption class="blockquote-footer">
+        Les produits sont remis soit :
+            En main Propre soit au travers de notre partenaire transport CoColis le transport responsable entre particuliers</li>.
+    </figcaption>
+</figure>
 <a href="www.cocolis.fr">Lien vers Cocolis</a>
 <img src="assets/footer.png" alt="" class="w-100">
 
