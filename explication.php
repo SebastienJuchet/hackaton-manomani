63285dca5aba074ac8b346ccb6a78c752245fa95
Aurélien <reyaurelien.business@gmail.com>
2022-01-19 19:31:30 +0100

    add explication page

--- explication.php
+++ explication.php
@@ -9,7 +9,106 @@
     <title>Document</title>
 </head>
 <body>
+<header>
+    <div class="text-center">
+        <img class="img-header" src="assets/segundoBandeau.png" alt="">
+    </div>
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto fw-bold text-uppercase">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Bricolage
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Jardinage
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            La communauté
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="https://blog.manomano.fr/">
+                            le blog ManoMano
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="https://www.manomano.fr/">
+                            le site ManoMano
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-search text-dark"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+<h2 class="h2 text-center">La marketplace Communautaire - Eco Responsable by ManoMano</h2>
+
+    <figure class="figure">
+        <blockquote class="blockquote">
+            <p class="text-mano font-size">Le principe</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Vous avez acheté un produit consommable chez Mano Mano et il vous en reste ! Plutôt que de le jeter ou le laisser dans le fond de votre garage,
+            nous vous proposons d'en faire profiter la communauté Mano Mano à un prix plafonné à 50% de son prix initial (hors promo).
+            Cette vente est comptabilisée dans votre cagnotte Ségundo Mano.
+        </figcaption>
+    </figure>
+
+    <figure class="figure">
+        <blockquote class="blockquote">
+            <p class="text-mano font-size">Comment utiliser ma cagnotte ?</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Vous avez une année pour utiliser la cagnotte sur le site ManoMano.
+        </figcaption>
+    </figure>
+
+    <figure class="figure">
+        <blockquote class="blockquote">
+            <p class="text-mano font-size">Comment se passe la livraison des produits vendus ?</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Les produits sont remis soit :
+                En main Propre soit au travers de notre partenaire transport CoColis le transport responsable entre particuliers</li>.
+        </figcaption>
+    </figure>
+<a href="www.cocolis.fr">Lien vers Cocolis</a>
+<img src="assets/footer.png" alt="" class="w-100">
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
