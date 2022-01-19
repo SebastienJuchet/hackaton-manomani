9580cbd30eac676010123368b4e7b261cab16ebf
Sebastien Juchet <sebastien.juchet@gmail.com>
2022-01-19 18:05:50 +0100

    corrige button

--- profil.php
+++ profil.php
@@ -11,14 +11,17 @@
 <body>
 <img src="assets/header.png" alt="" class="w-100">
 <div class="text-profil container-fluid d-flex h-50 align-items-center justify-content-center flex-wrap">
-    <div class="col-3 fs-2 mx-3 bg-white h-75 p-5">
+    <div class="col-3 fs-2 mx-3 bg-white h-50 p-5">
         <p class="text-profil">Vous avez trop ou pas assez: Profitez de l'offre <span class="text-mano">SECUNDO MANO</span></p>
     </div>
-    <div class="col-5">
-        <a href="secondo.php" class="btn btn-secundo h-75"></a>
+    <div class="col-3 bg-white div-link">
+        <a href="secondo.php" class="img-logo">
+            <img src="assets/logo.png" alt="" class="img mx-auto">
+        </a>
     </div>
     <div class="col-12 text-center fs-2 me-5"><a class="nav-link text-mano" href="explication.php">En savoir plus sur Secundo Mano</a></div>
 </div>
+<img src="assets/footer.png" alt="" class="w-100">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
