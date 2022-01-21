768d634e8b5e56f90a1bf67ec2dc5982115ca578
joffrey-gibouin <joffreygibouin@gmail.com>
2022-01-21 09:54:05 +0100

    add fivicon

--- component/head.php
+++ component/head.php
@@ -7,5 +7,6 @@
     <link rel="stylesheet" href="../styles.css">
     <script src="https://kit.fontawesome.com/acc585e2c3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="shortcut icon" href="../assets/fivicon.png">
     <title>Document</title>
 </head>
